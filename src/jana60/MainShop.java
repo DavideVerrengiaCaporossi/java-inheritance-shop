6ebec18d038c25d4f5cac6cdf70824c1b72064c1
DavideVerrengiaCaporossi <oren93vc@gmail.com>
2022-06-23 12:24:36 +0200

    sistmato main oggi creo il catologo dopo la correzione

--- src/jana60/MainShop.java
+++ src/jana60/MainShop.java
@@ -4,7 +4,10 @@ public class MainShop {
 public static void main(String[] args) {
 		
 		Smartphone iphone = new Smartphone(100,"Iphone","Apple",800.00, 22, "1231234241",64);
-System.out.println(iphone);
+		System.out.println(iphone.getNome() + iphone.getMarca() + iphone.getPrezzo() + iphone.getIva() + iphone.getImei() + iphone.getMemoria());
+		
+		
+       System.out.println(iphone);
 	}
 
 }
